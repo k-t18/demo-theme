21eddc4f9defb5ea5ace31a09a331670335181dd
karan <karan@8848digital.com>
2025-04-10 19:22:17 +0530

    stash

--- components/Navbar/NavbarMenuCenterAligned/MasterComponent.tsx
+++ components/Navbar/NavbarMenuCenterAligned/MasterComponent.tsx
@@ -4,37 +4,27 @@ import useNavbar from '../../../hooks/GeneralHooks/useNavbar';
 import useFetchCartItems from '../../../hooks/CartPageHook/useFetchCartItems';
 import useWishlist from '../../../hooks/WishlistHooks/useWishlistHook';
 import NavbarLayout from './NavbarLayout';
-import NavbarLoader from './NavbarLoader';
 
 function Navbar() {
   const { navbarData, isLoading, errorMessage, selectedCurrencyValue, handleLogoutUser, isLoggedIn } = useNavbar();
   const { selectedLanguageData }: any = useSelector(SelectedFilterLangDataFromStore);
   const { cartCount } = useFetchCartItems();
   const { wishlistCount } = useWishlist();
-
-  if (isLoading) {
-    return <NavbarLoader />;
-  } else if (navbarData?.length > 0) {
-    return (
-      <>
-        <NavbarLayout
-          navbarData={navbarData}
-          isLoading={isLoading}
-          errorMessage={errorMessage}
-          selectedCurrencyValue={selectedCurrencyValue}
-          handleLogoutUser={handleLogoutUser}
-          selectedLanguageData={selectedLanguageData}
-          cartCount={cartCount}
-          wishlistCount={wishlistCount}
-          isLoggedIn={isLoggedIn}
-        />
-      </>
-    );
-  } else if (errorMessage) {
-    return <></>;
-  } else {
-    return <></>;
-  }
+  return (
+    <>
+      <NavbarLayout
+        navbarData={navbarData}
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        selectedCurrencyValue={selectedCurrencyValue}
+        handleLogoutUser={handleLogoutUser}
+        selectedLanguageData={selectedLanguageData}
+        cartCount={cartCount}
+        wishlistCount={wishlistCount}
+        isLoggedIn={isLoggedIn}
+      />
+    </>
+  );
 }
 
 export default Navbar;
